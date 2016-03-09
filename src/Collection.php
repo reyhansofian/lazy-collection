ac35a399a1dc2f503406fdba05f3cee64ee6fb17
Prasetyo <jowy@oiry.net>
2016-03-09 21:02:33 +0700

    add phpdoc for collection

--- src/Collection.php
+++ src/Collection.php
@@ -5,41 +5,116 @@ namespace PrasWicaksono\LazyCollection;
 use Countable;
 use IteratorAggregate;
 
+/**
+ * Interface Collection
+ * @package PrasWicaksono\LazyCollection
+ */
 interface Collection extends Countable, IteratorAggregate
 {
+    /**
+     * @param callable $function
+     * @return Collection
+     */
     public function map(callable $function) : Collection;
 
+    /**
+     * @param callable $function
+     * @return Collection
+     */
     public function mapKeys(callable $function) : Collection;
 
+    /**
+     * @param callable $function
+     * @return Collection
+     */
     public function reindex(callable $function) : Collection;
 
+    /**
+     * @param callable $function
+     * @return void
+     */
     public function apply(callable $function);
 
+    /**
+     * @param callable $predicate
+     * @return Collection
+     */
     public function filter(callable $predicate) : Collection;
 
+    /**
+     * @param callable $function
+     * @param null $startValue
+     * @return mixed
+     */
     public function reduce(callable $function, $startValue = null);
 
+    /**
+     * @param callable $function
+     * @param null $startValue
+     * @return Collection
+     */
     public function reductions(callable $function, $startValue = null) : Collection;
 
+    /**
+     * @param array[]|\Traversable
+     * @return Collection
+     */
     public function zip(...$iterables) : Collection;
 
+    /**
+     * @param array[]|\Traversable
+     * @return Collection
+     */
     public function chain(...$iterables) : Collection;
 
+    /**
+     * @param array[]|\Traversable
+     * @return Collection
+     */
     public function product(...$iterables) : Collection;
 
+    /**
+     * @param int $num
+     * @return Collection
+     */
     public function take(int $num) : Collection;
 
+    /**
+     * @param int $num
+     * @return Collection
+     */
     public function drop(int $num) : Collection;
 
+    /**
+     * @param callable $function
+     * @return mixed
+     */
     public function search(callable $function);
 
+    /**
+     * @return Collection
+     */
     public function flatten() : Collection;
 
+    /**
+     * @return Collection
+     */
     public function flip() : Collection;
 
+    /**
+     * @param int $size
+     * @return Collection
+     */
     public function chunk(int $size) : Collection;
 
+    /**
+     * @param string $seperator
+     * @return string
+     */
     public function join(string $seperator) : string;
 
+    /**
+     * @return array
+     */
     public function toArray() : array;
 }
--- src/IterableCollection.php
+++ src/IterableCollection.php
@@ -2,51 +2,82 @@
 
 namespace PrasWicaksono\LazyCollection;
 
-use Generator;
-
+/**
+ * Class IterableCollection
+ * @package PrasWicaksono\LazyCollection
+ */
 final class IterableCollection implements Collection
 {
+    /**
+     * @var array|\Traversable
+     */
     private $elements;
 
+    /**
+     * @param $elements array|\Traversable
+     */
     private function __construct($elements)
     {
         \iter\_assertIterable($elements, 'Argument');
         $this->elements = $elements;
     }
 
+    /**
+     * @param array $elements
+     * @return IterableCollection
+     */
     public static function fromArray(array $elements) : IterableCollection
     {
         return new static($elements);
     }
 
-    public static function fromGenerator(Generator $elements) : IterableCollection
+    /**
+     * @param \Generator $elements
+     * @return IterableCollection
+     */
+    public static function fromGenerator(\Generator $elements) : IterableCollection
     {
         return new static($elements);
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function map(callable $function) : Collection
     {
         $this->elements = \iter\map($function, \iter\toIter($this->elements));
         return clone $this;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function mapKeys(callable $function) : Collection
     {
         $this->elements = \iter\mapKeys($function, \iter\toIter($this->elements));
         return clone $this;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function reindex(callable $function) : Collection
     {
         $this->elements = \iter\reindex($function, \iter\toIter($this->elements));
         return clone $this;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function apply(callable $function)
     {
         \iter\apply($function, \iter\toIter($this->elements));
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function filter(callable $predicate) : Collection
     {
         $this->elements = \iter\filter($predicate, \iter\toIter($this->elements));
@@ -54,29 +85,44 @@ final class IterableCollection implements Collection
         return clone $this;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function reduce(callable $function, $startValue = null)
     {
         return \iter\reduce($function, \iter\toIter($this->elements), $startValue);
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function reductions(callable $function, $startValue = null) : Collection
     {
         $this->elements = \iter\reductions($function, \iter\toIter($this->elements), $startValue);
         return clone $this;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function zip(...$iterables) : Collection
     {
         $this->elements = \iter\zip(\iter\toIter($this->elements), ...$iterables);
         return clone $this;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function chain(...$iterables) : Collection
     {
         $this->elements = \iter\chain(\iter\toIter($this->elements), ...$iterables);
         return clone $this;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function product(...$iterables) : Collection
     {
         $this->elements = \iter\chain(\iter\toIter($this->elements), ...$iterables);
@@ -89,50 +135,77 @@ final class IterableCollection implements Collection
         return clone $this;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function drop(int $num) : Collection
     {
         $this->elements = \iter\slice(\iter\toIter($this->elements), $num);
         return clone $this;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function search(callable $predicate)
     {
         return \iter\search($predicate, \iter\toIter($this->elements));
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function flatten() : Collection
     {
         $this->elements = \iter\flatten(\iter\toIter($this->elements));
         return clone $this;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function flip() : Collection
     {
         $this->elements = \iter\flip(\iter\toIter($this->elements));
         return clone $this;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function chunk(int $size) : Collection
     {
         $this->elements = \iter\chunk(\iter\toIter($this->elements), $size);
         return clone $this;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function join(string $separator) : string
     {
         return \iter\join($separator, \iter\toIter($this->elements));
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function count()
     {
         return count($this->toArray());
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getIterator()
     {
         return \iter\toIter($this->elements);
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function toArray() : array
     {
         return \iter\toArrayWithKeys($this->elements);
